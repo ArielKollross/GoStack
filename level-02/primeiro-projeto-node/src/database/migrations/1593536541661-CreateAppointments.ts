c441bffc8919a5df23c4059983e9d1ceff66e7b5
Ariel Kollross <arielkollross@gmail.com>
2020-07-01 11:13:25 -0300

    migrations update

--- level-02/primeiro-projeto-node/src/database/migrations/1593536541661-CreateAppointments.ts
+++ level-02/primeiro-projeto-node/src/database/migrations/1593536541661-CreateAppointments.ts
@@ -17,12 +17,10 @@ export default class CreateAppointments1593536541661 implements MigrationInterfa
             {
               name: 'provider',
               type: 'varchar',
-              isNullable: false,
             },
             {
               name: 'date',
               type: 'timestamp with time zone',
-              isNullable: false,
             },
           ],
         }),
