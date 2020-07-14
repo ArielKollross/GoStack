2c0a7205f573f0f314a7987f84a19a7c40194144
Ariel Kollross <arielkollross@gmail.com>
2020-07-14 12:02:28 -0300

    github dashboard style

--- level-03/github_explorer/src/pages/Dashboard/index.tsx
+++ level-03/github_explorer/src/pages/Dashboard/index.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title } from './styles';
+import githubLogo from '../../assets/githubLogo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 // define more easy typeOf Ts
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub.</Title>
+  return (
+    <>
+      <img src={githubLogo} alt="Github Explorer" />
+      <Title>Explore repositórios no GitHub.</Title>
 
+      <Form action="">
+        <input type="text" placeholder="Digite o nome do repositório"/>
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+          src="https://avatars0.githubusercontent.com/u/21115260?s=460&u=c1ba6a09cc170ffdc1a1cf9742964f4ad529d05e&v=4"
+          alt="Ariel Kollross"/>
+          <div>
+            <strong>ArielKollross/goStack</strong>
+            <p>solve exercices</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+          src="https://avatars0.githubusercontent.com/u/21115260?s=460&u=c1ba6a09cc170ffdc1a1cf9742964f4ad529d05e&v=4"
+          alt="Ariel Kollross"/>
+          <div>
+            <strong>ArielKollross/goStack</strong>
+            <p>solve exercices</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+          src="https://avatars0.githubusercontent.com/u/21115260?s=460&u=c1ba6a09cc170ffdc1a1cf9742964f4ad529d05e&v=4"
+          alt="Ariel Kollross"/>
+          <div>
+            <strong>ArielKollross/goStack</strong>
+            <p>solve exercices</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+
+    </>
+  )
 };
 
 export default Dashboard;
