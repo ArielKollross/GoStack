aa41903e07c5193cd1f105a9ac30c31e54a9944d
Ariel Kollross <arielkollross@gmail.com>
2020-07-18 00:52:23 -0300

    login page gobaber

---
+++ level-03/gobarber-web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+import SingIn from './pages/Signin';
+
+const App: React.FC = () => (
+  <>
+    <SingIn />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
