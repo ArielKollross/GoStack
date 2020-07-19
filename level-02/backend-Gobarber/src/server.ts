99af6c737032432e32e597a5c2f69f5ca073df39
Ariel Kollross <arielkollross@gmail.com>
2020-07-19 02:45:17 -0300

    configured cors

--- level-02/backend-Gobarber/src/server.ts
+++ level-02/backend-Gobarber/src/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 
 import express, {Request, Response, NextFunction} from 'express';
+import cros from 'cors';
+
 import 'express-async-errors';
 
 import routes from './routes';
@@ -11,6 +13,7 @@ import './database';
 
 const app = express();
 
+app.use(cros()); // {origin: 'http://localhost/3000'}
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
