16e3ab0c99583321fcef11bd1ead15644a372112
Ariel Kollross <arielkollross@gmail.com>
2020-07-19 02:38:19 -0300

    login, register page GoBarber and changed name of backend project

--- level-03/gobarber-web/src/App.tsx
+++ level-03/gobarber-web/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import SingIn from './pages/Signin';
+// import SingIn from "./pages/SignIn";
+import SingIn from './pages/SignIn';
 
 const App: React.FC = () => (
   <>
