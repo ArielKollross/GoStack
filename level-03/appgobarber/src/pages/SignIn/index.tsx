58c8e7c8f7eca5d03517dc70fe9b1bb0f86ab177
Ariel Kollross <arielkollross@gmail.com>
2020-07-31 01:46:55 -0300

    mobile login screnn

--- level-03/appgobarber/src/pages/SignIn/index.tsx
+++ level-03/appgobarber/src/pages/SignIn/index.tsx
@@ -1,17 +1,63 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+      >
+      <ScrollView
+        contentContainerStyle={{flex: 1}}
+        keyboardShouldPersistTaps="handled"
+      >
+      <Container>
+        <Image source={logoImg} />
 
-      <Title>Faça seu logon</Title>
-    </Container>
+        <View>
+          <Title>Faça seu logon</Title>
+        </View>
+
+        <Input name="email" icon="mail" placeholder="E-mail" />
+        <Input name="password" icon="lock" placeholder="Senha" />
+
+        <Button
+          onPress={() => {
+            console.log('hi');
+          }}
+        >
+          Entrar
+        </Button>
+
+        <ForgotPassword onPress={() => {}}>
+          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
+      </Container>
+      </ScrollView>
+
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 
