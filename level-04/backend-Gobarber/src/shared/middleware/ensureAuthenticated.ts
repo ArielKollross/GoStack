62acee09f5212be566e32d0d77ab0c988f6c7692
Ariel Kollross <arielkollross@gmail.com>
2020-08-15 15:49:13 -0300

    aplay DDD concepts to the goBarber backend

---
+++ level-04/backend-Gobarber/src/shared/middleware/ensureAuthenticated.ts
@@ -0,0 +1,42 @@
+import {Request, Response, NextFunction} from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
+
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  request: Request, response: Response, next: NextFunction): void {
+    //token JWT authentication
+
+    const authHeader = request.headers.authorization;
+
+    if(!authHeader) {
+      throw new AppError('JWT token is missing', 401);
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    try{
+      const decoded = verify(token, authConfig.jwt.secret);
+
+      const { sub } = decoded as TokenPayload;
+
+      // inteser user.id on Request express method
+      request.user = {
+        id: sub,
+      }
+
+      console.log(decoded);
+
+      return next();
+    } catch (err) {
+      throw new AppError('Invalid JWT token', 401);
+    }
+}
---
+++ level-04/backend-Gobarber/src/shared/routes/appointments.routes.ts
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentServices from '../services/CreateAppointmentServices';
+
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
+
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+  const appointments = await appointmentsRepository.find();
+
+  return response.json(appointments);
+})
+
+appointmentsRouter.post('/', async (request, response) => {
+
+  const { provider_id, date } = request.body;
+
+  const parsedDate = parseISO(date);
+
+  const createAppointment = new CreateAppointmentServices();
+
+  const appointment = await createAppointment.execute({
+    date: parsedDate,
+    provider_id,
+  });
+
+  return response.json(appointment);
+
+});
+
+export default appointmentsRouter;
