7e7334935345376f130a1387fe65feca7d373ae9
Ariel Kollross <arielkollross@gmail.com>
2020-09-01 00:41:57 -0300

    add lib class-transformer

--- level-04/backend-Gobarber/src/modules/users/infra/typeorm/entities/User.ts
+++ level-04/backend-Gobarber/src/modules/users/infra/typeorm/entities/User.ts
@@ -4,6 +4,8 @@ PrimaryGeneratedColumn,
 CreateDateColumn,
 UpdateDateColumn } from 'typeorm';
 
+import { Exclude, Expose } from 'class-transformer';
+
 @Entity('users')
 class Users {
   @PrimaryGeneratedColumn('uuid')
@@ -16,9 +18,11 @@ class Users {
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
 
   @Column()
+  @Exclude()
   avatar: string;
 
   @CreateDateColumn()
@@ -26,6 +30,12 @@ class Users {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @Expose({ name: 'avatar_url' })
+  getAvatarUrl(): string | null {
+    return this.avatar ?
+     `${process.env.APP_API_URL}/files/${this.avatar}` : null;
+  }
 }
 
 export default Users;
