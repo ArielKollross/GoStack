4b2d1b11e94cdbcd81d837638a6f84ba785ee45a
Ariel Kollross <arielkollross@gmail.com>
2020-09-14 10:45:35 -0300

    fix/ list provider send data

--- level-04/backend-Gobarber/src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ level-04/backend-Gobarber/src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import ListProviderService from '@modules/appointments/services/ListProviderService';
+import { classToClass } from 'class-transformer';
 
 export default class ProviderController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -13,6 +14,6 @@ export default class ProviderController {
     user_id,
   });
 
-  return response.json(providers);
+  return response.json(classToClass(providers));
   }
 }
